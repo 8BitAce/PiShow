5eee0d2de216787b8d574fcd4ed734d58864fd96
unknown <kscheiw1@IS-6KYMFJ1-D.dpu.depaul.edu>
2014-11-21 13:16:19 -0600

    Add command-line image dir arg.

--- pishow.py
+++ pishow.py
@@ -46,47 +46,52 @@ class DropboxConnector:
                 files.append(('%s' % name).encode(encoding))
             return files
 
-    def get_file(self, filename):
-        to_file = open(os.path.expanduser("Images/" + filename), "wb")
+    def get_file(self, filename, directory):
+        to_file = open(os.path.expanduser(self.local_directory + filename), "wb")
 
         f, metadata = self.api_client.get_file_and_metadata(self.current_path + "/" + filename)
         to_file.write(f.read())
 
 class Slideshow:
-    def __init__(self, dbc):
-        self.file_set = set([f for f in os.listdir("Images") if os.path.isfile(os.path.join("Images",f))])
+    def __init__(self, dbc, local_dir):
+        self.file_set = set([f for f in os.listdir(self.local_directory) if os.path.isfile(os.path.join(self.local_directory,f))])
         self.dbc = dbc
-        self.directory = "Photos/Sample Album"
+        self.remote_directory = "Photos/Sample Album"
+        self.local_directory = local_dir
 
     def run_show(self, delay):
         self.update_files()
-        child = subprocess.Popen(["feh", "-FY", "-D", str(delay), "Images"])
+        child = subprocess.Popen(["feh", "-FY", "-D", str(delay), self.local_directory])
         while(True):
             if(self.update_files()):
                 child.kill()
-                child = subprocess.Popen(["feh", "-FY", "-D", str(delay), "Images"])
+                child = subprocess.Popen(["feh", "-FY", "-D", str(delay), self.local_directory])
             sleep(60)
 
     def update_files(self):
         """Returns True if fileset changed"""
-        db_files = self.dbc.get_file_list(self.directory)
+        db_files = self.dbc.get_file_list(self.remote_directory)
         new_files = set(db_files) - self.file_set
         old_files = self.file_set - set(db_files)
         if new_files != set() or old_files != set():
             self.file_set = set(db_files)
             for filename in new_files:
-                self.dbc.get_file(filename)
+                self.dbc.get_file(filename, self.local_directory)
             for filename in old_files:
-                os.remove("Images/" + filename)
+                os.remove(self.local_directory + filename)
             print "Fileset changed."
             print self.file_set
             return True
         return False
 
-def main():
+def main(argv):
+    if(argv == None):
+        print "Usage: pishow.py <local_image_directory>"
+        return
+    local_directory = argv + "/" if argv[-1] != "/" else argv
     dbc = DropboxConnector(APP_KEY, APP_SECRET)
-    slideshow = Slideshow(dbc)
+    slideshow = Slideshow(dbc, local_dir)
     slideshow.run_show(DELAY)
 
 if __name__ == "__main__":
-	main()
+	main(argv[1])
