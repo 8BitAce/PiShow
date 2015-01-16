06c0d075c4eedce3e8b288994e3c5d4708c8e7e4
Keith Scheiwiller <k.scheiw@gmail.com>
2015-01-16 19:28:12 +0000

    Typo.

--- pishow.py
+++ pishow.py
@@ -17,7 +17,7 @@ def main(argv):
         print "       pishow.py auth"
         return
     if(argv[1] == "auth"):
-    	dropboxconnector.do_login()
+    	DropboxConnector.do_login()
     	return
     local_directory = argv[1] + "/" if argv[1][-1] != "/" else argv[1]
     db_directory = argv[2] + "/" if argv[2][-1] != "/" else argv[2]
