122edfc192e2ff9b09e5f2980cd4c52edd0c4d7d
Keith Scheiwiller <k.scheiw@gmail.com>
2015-02-27 17:20:00 +0000

    Absolute log path.

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 X -nolisten tcp > /dev/null 2>&1 &
-DISPLAY=:0.0 /usr/bin/python2 -u pishow.py /home/pi/pishow/Images Photos/Sample\ Album > log.txt 2>&1 &
+DISPLAY=:0.0 /usr/bin/python2 -u pishow.py /home/pi/pishow/Images Photos/Sample\ Album > /home/pi/pishow/log.txt 2>&1 &
