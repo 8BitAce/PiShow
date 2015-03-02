2468abe834bf30af4856bc5dba0a5edf95609f49
Keith Scheiwiller <k.scheiw@gmail.com>
2015-03-02 21:12:39 +0000

    Folder is raspberrypi01.

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 X -nolisten tcp > /dev/null 2>&1 &
 DISPLAY=:0.0 xset -dpms; DISPLAY=:0.0 xset s off
-DISPLAY=:0.0 /usr/bin/python2 -u /home/pi/pishow/pishow.py /home/pi/pishow/Images Photos/Sample\ Album > /home/pi/pishow/log.txt 2>&1 &
+DISPLAY=:0.0 /usr/bin/python2 -u /home/pi/pishow/pishow.py /home/pi/pishow/Images $HOSTNAME > /home/pi/pishow/log.txt 2>&1 &
