c529de98d7a008e1fe5913e08e007d4049179c68
Keith Scheiwiller <k.scheiw@gmail.com>
2015-05-07 15:05:23 +0000

    pi->pishow

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 X -nolisten tcp > /dev/null 2>&1 &
 DISPLAY=:0.0 xset -dpms; DISPLAY=:0.0 xset s off
-DISPLAY=:0.0 /usr/bin/python2 -u -m /home/pi/pishow/pishow/ /home/pi/pishow/Images $HOSTNAME > /home/pi/pishow/log.txt 2>&1 &
+DISPLAY=:0.0 /usr/bin/python2 -u /home/pishow/pishow/pishow/__main__.py /home/pishow/pishow/Images $HOSTNAME > /home/pishow/pishow/log.txt 2>&1 &
